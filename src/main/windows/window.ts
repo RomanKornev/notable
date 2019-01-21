9b90b119d972f11c1b5baf585a439e2940f6b7b7
Roman Kornev <w.romankornev@gmail.com>
2019-01-21 18:00:44 +0300

    [FIX] White flash workaround

--- src/main/windows/window.ts
+++ src/main/windows/window.ts
@@ -137,7 +137,7 @@ class Window {
 
     options = _.merge ( dimensions, {
       frame: !is.macOS (),
-      backgroundColor: '#fdfdfd',
+      backgroundColor: '#181818',
       icon: path.join ( __static, 'images', `icon.${is.windows () ? 'ico' : 'png'}` ),
       show: false,
       title: pkg.productName,
@@ -145,10 +145,15 @@ class Window {
       webPreferences: {
         nodeIntegration: true,
         webSecurity: false
-      }
+      },
+      width: 0,
+      height: 0,
+      x: 0,
+      y: 0
     }, options );
 
     const win = new BrowserWindow ( options );
+    win.show ();
 
     state.manage ( win );
 
